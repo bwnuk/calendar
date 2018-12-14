23f599ab59338c2f823b3c07b6dd32da08a5b2c7
Bartłomiej Wnuk <bartekwnuk13@gmail.com>
2018-12-14 20:45:03 +0100

    Default calendar look. Start works.

---
+++ app/src/main/java/pl/krakow/politechnika/io/wnuk/calendar/MainActivity.java
@@ -0,0 +1,54 @@
+package pl.krakow.politechnika.io.wnuk.calendar;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.github.sundeepk.compactcalendarview.CompactCalendarView;
+import com.github.sundeepk.compactcalendarview.domain.Event;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final CompactCalendarView compactCalendarView = (CompactCalendarView) findViewById(R.id.compactcalendar_view);
+        // Set first day of week to Monday, defaults to Monday so calling setFirstDayOfWeek is not necessary
+        // Use constants provided by Java Calendar class
+        compactCalendarView.setFirstDayOfWeek(Calendar.MONDAY);
+
+        // Add event 1 on Sun, 07 Jun 2015 18:20:51 GMT
+        Event ev1 = new Event(Color.GREEN, 1433701251000L, "Some extra data that I want to store.");
+        compactCalendarView.addEvent(ev1);
+
+        // Added event 2 GMT: Sun, 07 Jun 2015 19:10:51 GMT
+        Event ev2 = new Event(Color.GREEN, 1433704251000L);
+        compactCalendarView.addEvent(ev2);
+
+        // Query for events on Sun, 07 Jun 2015 GMT.
+        // Time is not relevant when querying for events, since events are returned by day.
+        // So you can pass in any arbitary DateTime and you will receive all events for that day.
+        List<Event> events = compactCalendarView.getEvents(1433701251000L); // can also take a Date object
+
+        // events has size 2 with the 2 events inserted previously
+
+        // define a listener to receive callbacks when certain events happen.
+        compactCalendarView.setListener(new CompactCalendarView.CompactCalendarViewListener() {
+            @Override
+            public void onDayClick(Date dateClicked) {
+                List<Event> events = compactCalendarView.getEvents(dateClicked);
+            }
+
+            @Override
+            public void onMonthScroll(Date firstDayOfNewMonth) {
+            }
+        });
+
+    }
+}
--- app/src/main/java/pl/krakow/politechnika/io/wnuk/calendar/StartActivity.java
+++ app/src/main/java/pl/krakow/politechnika/io/wnuk/calendar/StartActivity.java
@@ -1,13 +1,44 @@
 package pl.krakow.politechnika.io.wnuk.calendar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class StartActivity extends AppCompatActivity {
+
+    private TextView tv_welcome;
+    private ImageView iv_icon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        tv_welcome = (TextView) findViewById(R.id.tv_welcome);
+        iv_icon = (ImageView) findViewById(R.id.iv_icon_earth);
+
+
+        /**
+         * Start activity opening and timing
+         */
+        final Intent intent = new Intent(this, MainActivity.class);
+
+        Thread timer = new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                try{
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }finally {
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+        timer.start();
     }
 }
